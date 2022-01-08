3687873986599003e862f918acb08c430d4ad3a7
aviadamd <aviadamd@gmail.com>
2022-01-08 13:55:33 +0200

    fix

--- src/main/java/base/Base.java
+++ src/main/java/base/Base.java
@@ -1,4 +1,26 @@
 package base;
 
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import lombok.extern.slf4j.Slf4j;
+import net.lightbody.bmp.BrowserMobProxy;
+import org.openqa.selenium.Proxy;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeClass;
+import ru.yandex.qatools.ashot.Screenshot;
+
+@Slf4j
 public class Base {
+
+    public static WebDriver driver;
+    public static Proxy seleniumProxy;
+    public static BrowserMobProxy mobProxy;
+    public static AppiumDriverLocalService server;
+    public static PropertyConfig getProperty;
+    public static Screenshot imageScreenShot;
+
+    @BeforeClass
+    public void initClass() {
+        String path = "/src/main/resources/config.properties";
+        getProperty = new PropertyConfig(path);
+    }
 }
